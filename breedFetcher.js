821f708884b43e0f64c3d0a56d6fb36cbb122662
Abdulahi hirsi <abdullahi.hirsi@live.com>
2022-09-29 22:23:36 +0000

    refactored code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,19 @@
 const request = require('request');
 input = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${input}`, (error, response, body) => {
-  if (error){
-    console.log('error: ', error)
-  }
-  const data = JSON.parse(body);
-  if (data === undefined){
-    console.log("Breed not found!")
-  }
-  console.log(data)
-})  
+const fetchBreedDescription = function(breedName, callback) {
+
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${input}`, (error, response, body) => {
+    if (error){
+      callback('error: ', error)
+    }
+    const data = JSON.parse(body);
+    if (data === undefined){
+      callback("Breed not found!")
+    }
+    callback(data)
+  })  
+};
   
 
-
+module.exports = { fetchBreedDescription };
